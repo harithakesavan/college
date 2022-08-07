f34d0ca0e40586b65f3961683a07e1ebd5f47412
harithakesavan <harithakesavan58@gmail.com>
2022-08-07 16:32:33 +0530

    college

---
+++ college.js
@@ -0,0 +1,60 @@
+function login(){
+    window.location="home.html"
+    alert("login successfull")
+}
+function register(){
+    window.location="index.html"
+    alert("registerd successfully")
+}
+function logout(){
+    window.location="index.html"
+    alert("logout successfull")
+}
+function study(){
+    window.location="students.html"
+}
+function marklist(){
+    window.location="marklist.html"
+}
+function mark(){
+    window.location="mark.html"
+}
+function pay(){
+    alert("payment successfull,receipt will be send to the mobile number")
+    window.location="home.html"
+}
+function fee(){
+    window.location="fee.html"
+}
+function attend(){
+    window.location="attend.html"
+}
+function attend1(){
+    window.location="attend1.html"
+}
+function fac(){
+    window.location="fdetails.html"
+
+}
+function depart(){
+    window.location="depart.html"
+}
+function bus(){
+    window.location="bus.html"
+}
+function busfee(){
+    window.location="busfee.html"
+}
+function order(){
+    window.location="order.html"
+}
+function or(){
+    alert("order successfull,food will be deliverd within 30 minutes")
+    window.location="home.html"
+}
+function work(){
+    window.location="worker.html"
+}
+
+
+
